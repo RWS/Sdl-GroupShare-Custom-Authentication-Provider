7f0da7d110a7a76448b294131efd368b820ffd64
Romulus Crisan <cromica@gmail.com>
2015-10-29 10:37:49 +0200

    initial commit of SDL GroupShare custom authentication provider

---
+++ CSV Authentication Provider/CSVAuthenticationProvider.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Sdl.StudioServer.Api.Core;
+
+namespace CsvAuthenticationProvider
+{
+    [CustomAuthenticationProviderExtention(CanValidateUserExistance = true)]
+    public class CsvAuthenticationProvider: ICustomAuthenticationProvider
+    {
+        private readonly Dictionary<string, string> _users;
+
+        public CsvAuthenticationProvider()
+        {
+            _users =  new Dictionary<string, string>();
+            this.LoadUsers();
+        }
+
+        private void LoadUsers()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using (var stream = assembly.GetManifestResourceStream("CsvAuthenticationProvider.Resources.users.csv"))
+                if (stream != null)
+                    using (var reader = new StreamReader(stream))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            var line = reader.ReadLine();
+                            if (line == null) continue;
+                            var strArrays = line.Split(',');
+                            if (_users.ContainsKey(strArrays[0]))
+                            {
+                                continue;
+                            }
+                            _users.Add(strArrays[0].ToLower(), strArrays[1]);
+                        }
+                    }
+        }
+
+        public bool UserExists(string userName)
+        {
+            return _users.ContainsKey(userName.ToLower());
+        }
+
+        public bool ValidateCredentials(string userName, string password)
+        {
+            if (!_users.ContainsKey(userName))
+            {
+                return false;
+            }
+            string savedPassword;
+            _users.TryGetValue(userName.ToLower(), out savedPassword);
+            return savedPassword == password;
+        }
+    }
+}
---
+++ CsvAuthenticationProvider.Test/CsvAuthenticationProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsvAuthenticationProvider.Test
+{
+    [TestClass]
+    public class CsvAuthenticationProviderTest
+    {
+        [TestMethod]
+        public void Check_If_User_Exists()
+        {
+            CsvAuthenticationProvider provider = new CsvAuthenticationProvider();
+            var expected = true;
+            var actual = provider.UserExists("User");
+
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
